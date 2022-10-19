8a2c1c59445bebd528e1f36b6beb2a3d5102c2fa
alamin <amin.se.aiub@gmail.com>
2022-10-19 13:39:31 +0600

    session example

---
+++ php/login/regCheck.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    if($username == "" || $password == "" || $email == ""){
+        //echo "null username/password/email";
+        header('location: signup.php?err=null');
+
+    }else {
+        if(isset($_SESSION['users'])){
+            $_SESSION['users'] = [];
+            $user = ['username'=> $username, "password"=> $password, "email"=> $email];
+            $users = $_SESSION['users'];
+            array_push($users, $user);
+            $_SESSION['users'] = $users;
+        }else{
+            $users = [];
+            $user = ['username'=> $username, "password"=> $password, "email"=> $email];
+            $users = $_SESSION['users'];
+            array_push($users, $user);
+            $_SESSION['users'] = $users;
+        }
+
+        print_r($users);
+
+        //header('location: login.php');
+    }
+
+
+?>
