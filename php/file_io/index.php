b5b597faf0e6c9dc4dd0749845e9d8d386eccd07
alamin <amin.se.aiub@gmail.com>
2022-10-25 15:19:33 +0600

    file I/O

---
+++ php/file_io/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    // $file = fopen('user.txt', 'r');
+    // $data = fread($file, filesize('user.txt'));
+    // echo $data;
+    // fclose($file);
+
+    // $file = fopen('user.txt', 'w');
+    // $data = "New line...";
+    // fwrite($file, $data);
+    // fclose($file);
+    // echo "Done";
+
+    // $file = fopen('user.txt', 'a');
+    // $data = "New line...\r\n";
+    // fwrite($file, $data);
+    // fclose($file);
+    // echo "Done";
+
+?>
