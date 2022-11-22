72966d86c02fc4f9d1ad97104d978fdc680c12ab
alamin <amin.se.aiub@gmail.com>
2022-11-22 15:52:05 +0600

    JS example + CSS position

---
+++ css/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <title>CSS Example</title>
+    <link href="style.css" rel="stylesheet"/>
+</head>
+
+<body>
+    <div id="d1">
+        <div id="d2"></div>
+    </div>
+</body>
+</html>
